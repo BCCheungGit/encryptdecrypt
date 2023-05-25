93c030e0d196b034f8302bdaa25ad1f6a8223dc8
Ben Cheung <87406737+BCCheungGit@users.noreply.github.com>
2023-05-24 21:15:04 -0400

    finished

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, render_template, flash, request, redirect, url_for, Flask
+from cryptography.fernet import Fernet
+from time import sleep
+import os,sys,time,datetime,getopt,csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asdfasdfasdf'
+
+def encrypt1(plain_text):
+    key = Fernet.generate_key() #this is your "password"
+    cipher_suite = Fernet(key)
+    encrypted = cipher_suite.encrypt(str.encode(plain_text))
+    return encrypted, key
+
+def decrypt1(encoded_text, key):
+    cipher_suite = Fernet(key)
+    decrypted = cipher_suite.decrypt(encoded_text)
+    return decrypted
+
+
+def get_key(message):
+    return encrypt(message)
+
+def timeNow():
+    return str(datetime.datetime.strftime(datetime.datetime.now(), "%H:%M:%S") + "======== ")
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/encrypt', methods=['GET', 'POST'])
+def encrypt():
+    if request.method == 'POST':
+        input = request.form.get('message')
+        if len(sys.argv) == 1:
+            print(request.form.get('message'))
+            print("Message to be encrypted:", input)
+            encrypted_msg, encryption_key = encrypt1(input)
+
+        return render_template('index.html', outmessage=encrypted_msg.decode('utf-8'), outkey=encryption_key.decode('utf-8'))
+    return render_template('index.html')
+
+
+
+@app.route('/decrypt', methods=['GET', 'POST'])
+def decrypt():
+    if request.method == 'POST':
+        if len(sys.argv) == 1:
+            encoded = request.form.get('encoded')
+            key = request.form.get('key')
+            decrypted_message = decrypt1(encoded, key).decode('utf-8')
+            return render_template('index.html', original=decrypted_message) 
+    return render_template('index.html', original=decrypted_message)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
